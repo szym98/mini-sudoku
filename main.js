3bc5b42acf30e817b35faf9433beeba8427340c5
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-01-06 21:39:54 +0100

    demo mini

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const board =document.querySelector('#puzzle')
 const solve=document.querySelector('#solve-b')
 const squares = 81
+
+
+for (let i = 0; i < squares; i++) {
+    const inputElement = document.createElement('input')
+    inputElement.setAttribute('type', 'number')
+    inputElement.setAttribute('min', '0')
+    inputElement.setAttribute('max', '9') }
