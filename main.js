189a33f5c6b1a92c6f2c3ec2238f7ee3ffb8d8bf
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-01-07 19:55:32 +0100

    demo mini

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const board =document.querySelector('#puzzle')
 const solve=document.querySelector('#solve-b')
+
 const squares = 81
-
-
 for (let i = 0; i < squares; i++) {
     const inputElement = document.createElement('input')
     inputElement.setAttribute('type', 'number')
     inputElement.setAttribute('min', '0')
-    inputElement.setAttribute('max', '9') }
+    inputElement.setAttribute('max', '9')
+    if (
+        ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i < 21) ||
+        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i < 27) ||
+        ((i % 9 === 3 || i % 9 === 4 || i % 9 === 5) && (i > 27 && i < 53)) ||
+        ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i > 53) ||
+        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i > 53)
+    ) {
+        inputElement.classList.add('odd-section')
+    }
+    puzzle.appendChild(inputElement)
+}
+
+
