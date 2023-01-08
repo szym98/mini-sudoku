8595f6e74f128138273f3b75cd43eb76cf141b65
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-01-08 17:58:23 +0100

    demo mini

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const puzzleBoard = document.querySelector('#puzzle')
-const solveButton = document.querySelector('#solve-button')
-const explainerDisplay = document.querySelector('#explainer')
+const solveButton = document.querySelector('#solve-b')
+const explainerDis = document.querySelector('#explainer')
 const area = 81
 let submission = []
 
@@ -9,13 +9,7 @@ for (let i = 0; i < area; i++) {
     inputElement.setAttribute('type', 'number')
     inputElement.setAttribute('min', '0')
     inputElement.setAttribute('max', '9')
-    if (
-        ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i < 21) ||
-        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i < 27) ||
-        ((i % 9 === 3 || i % 9 === 4 || i % 9 === 5) && (i > 27 && i < 53)) ||
-        ((i % 9 === 0 || i % 9 ===1 || i % 9 === 2) && i > 53) ||
-        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i > 53)
-    ) {
+    {
         inputElement.classList.add('odd-section')
     }
     puzzleBoard.appendChild(inputElement)
