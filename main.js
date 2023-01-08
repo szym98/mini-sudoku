69521a64022922c420acc4ce7991cc1a46829b00
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-01-08 14:17:28 +0100

    demo mini

--- main.js
+++ main.js
@@ -1,22 +1,112 @@
-const board =document.querySelector('#puzzle')
-const solve=document.querySelector('#solve-b')
+const game = document.getElementById('game')
+const scoreDisplay = document.getElementById('score')
+let score = 0
 
-const squares = 81
-for (let i = 0; i < squares; i++) {
-    const inputElement = document.createElement('input')
-    inputElement.setAttribute('type', 'number')
-    inputElement.setAttribute('min', '0')
-    inputElement.setAttribute('max', '9')
-    if (
-        ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i < 21) ||
-        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i < 27) ||
-        ((i % 9 === 3 || i % 9 === 4 || i % 9 === 5) && (i > 27 && i < 53)) ||
-        ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i > 53) ||
-        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i > 53)
-    ) {
-        inputElement.classList.add('odd-section')
+const genres = [
+    {
+        name: 'Books',
+        id: 10
+    },
+    {
+        name: 'Film',
+        id: 11
+    },
+    {
+        name: 'Music',
+        id: 12
+    },
+    {
+        name: 'Video Games',
+        id: 15
     }
-    puzzle.appendChild(inputElement)
+]
+
+const levels = ['easy', 'medium', 'hard']
+
+function addGenre(genre) {
+    const column = document.createElement('div')
+    column.classList.add('genre-column')
+    column.innerHTML = genre.name
+    game.append(column)
+
+    levels.forEach(level => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        column.append(card)
+
+        if (level === 'easy') {
+            card.innerHTML = 100
+        }
+        if (level === 'medium') {
+            card.innerHTML = 200
+        }
+        if (level === 'hard') {
+            card.innerHTML = 300
+        }
+
+        fetch(https://opentdb.com/api.php?amount=10&category=19&difficulty=medium&type=boolean`)
+            .then(response => response.json())
+            .then(data => {
+                // Not enough data to get 10 items per difficulty level and get a random
+                // question using Math.random() and passing it through instead of 0
+                // eg: if amount=10 above, you could do:
+                // cont randomNumber = Math.floor(Math.random() * 10)
+                // and pass through randomNumber, so:
+                // data.results[randomNumber].question
+                console.log(data)
+                card.setAttribute('data-question', data.results[0].question)
+                card.setAttribute('data-answer', data.results[0].correct_answer)
+                card.setAttribute('data-value', card.getInnerHTML())
+            })
+            .then(done => card.addEventListener('click', flipCard))
+
+    })
+}
+genres.forEach(genre => addGenre(genre))
+
+
+function flipCard() {
+    this.innerHTML = ''
+    this.style.fontSize = '15px'
+    const textDisplay = document.createElement('div')
+    const trueButton = document.createElement('button')
+    const falseButton = document.createElement('button')
+    trueButton.innerHTML = 'True'
+    falseButton.innerHTML = 'False'
+    trueButton.classList.add('true-button')
+    falseButton.classList.add('false-button')
+    trueButton.addEventListener('click', getResult)
+    falseButton.addEventListener('click', getResult)
+    textDisplay.innerHTML = this.getAttribute('data-question')
+    this.append(textDisplay, trueButton, falseButton)
+
+    const allCards = Array.from(document.querySelectorAll('.card'))
+    allCards.forEach(card => card.removeEventListener('click', flipCard))
 }
 
+function getResult() {
+    const allCards = Array.from(document.querySelectorAll('.card'))
+    allCards.forEach(card => card.addEventListener('click', flipCard))
 
+    const cardOfButton = this.parentElement
+    if (cardOfButton.getAttribute('data-answer') === this.innerHTML) {
+        score = score + parseInt(cardOfButton.getAttribute('data-value'))
+        scoreDisplay.innerHTML = score
+        cardOfButton.classList.add('correct-answer')
+        setTimeout(() => {
+            while (cardOfButton.firstChild) {
+                cardOfButton.removeChild(cardOfButton.lastChild)
+            }
+            cardOfButton.innerHTML = cardOfButton.getAttribute('data-value')
+        }, 100)
+    } else {
+        cardOfButton.classList.add('wrong-answer')
+        setTimeout(() => {
+            while (cardOfButton.firstChild) {
+                cardOfButton.removeChild(cardOfButton.lastChild)
+            }
+            cardOfButton.innerHTML = 0
+        }, 100)
+    }
+    cardOfButton.removeEventListener('click',flipCard)
+}
