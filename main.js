afb7aabdc2f143554b01d0245dbe8f9f73eb31d1
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-01-09 12:59:03 +0100

    demo mini

--- main.js
+++ main.js
@@ -9,8 +9,13 @@ for (let i = 0; i < area; i++) {
     inputElement.setAttribute('type', 'number')
     inputElement.setAttribute('min', '0')
     inputElement.setAttribute('max', '9')
-    {
-        inputElement.classList.add('odd-section')
+    { if (
+        ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i < 21) ||
+        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i < 27) ||
+        ((i % 9 === 3 || i % 9 === 4 || i % 9 === 5) && (i > 27 && i < 53)) ||
+        ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i > 53) ||
+        ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i > 53)
+    )
+        inputElement.classList.add('oddSection')
     }
-    puzzleBoard.appendChild(inputElement)
-}
+    puzzleBoard.appendChild(inputElement)}
